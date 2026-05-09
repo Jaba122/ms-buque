b6b11ffa4681e839559122a00e3fc96e764bfab4
Jaba122 <ja.nunezo@duoc.cl>
2026-05-08 21:15:36 -0400

    feat: estructura base microservicio Buque con Spring Boot 3.4.5, JPA, MySQL y Lombok

---
+++ Buque/src/test/java/com/CordyTech/Buque/BuqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CordyTech.Buque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
